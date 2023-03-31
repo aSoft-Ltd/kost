195c4844e49737f5b638f625f81cfea803a2addf
George Sechu <admin@mitikaz.com>
2023-03-31 15:22:17 +0300

    Before auto merging main

--- core/src/commonMain/kotlin/kost/PaymentApi.kt
+++ core/src/commonMain/kotlin/kost/PaymentApi.kt
@@ -4,9 +4,7 @@ import kost.params.PaymentParams
 import koncurrent.Later
 
 interface PaymentApi {
-    fun create(params: PaymentParams): Later<Payment>
+    fun append(params: PaymentParams): Later<Payment>
     fun refund(params: PaymentParams): Later<Payment>
-//    fun update(uid: String, params: PaymentParams): Payment
-//    fun delete(uid: String): Payment
-//    fun deleteBulk(uids: List<String>): List<Payment>
+    fun cancel(uid: String): Later<Payment>
 }
